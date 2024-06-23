c1c252f0d6052f02b2d49d5b3150cd6624f32a41
TakanariShimbo <takanari.shimbo@gmail.com>
2024-06-24 00:58:38 +0900

    add logo

--- model/conbined_image.py
+++ model/conbined_image.py
@@ -5,6 +5,7 @@ from .size import Size
 
 FONT_PATH = "./data/DejaVuSans.ttf"
 HEART_IMAGE_PATH = "./data/heart.png"
+LOGO_IMAGE_PATH = "./data/logo.png"
 
 
 class ConbinedImage:
@@ -18,7 +19,7 @@ class ConbinedImage:
         margin: int = 80,
         bg_color: str = "#FFFFFF",
         text_color: str = "#000000",
-        logo_text: str = "Created with #ABCDEFG",
+        logo_text: str = "ABCDEFG",
     ) -> None:
         max_size = Size.from_tuple(size=image_size)
         max_size_l = Size(width=max_size.w * image_ratios[0] / sum(image_ratios), height=max_size.h)
@@ -31,12 +32,14 @@ class ConbinedImage:
         image_rgba_1 = image_rgb_1.convert(mode="RGBA")
         image_rgba_2 = image_rgb_2.convert(mode="RGBA")
         heart_image_rgba = self._get_heart_image(percent_value=percent_value, text_color=bg_color)
-        text_image_rgba = self._get_text_image(text=logo_text, text_color=text_color)
+        logo_icon_image_rgba = self._get_logo_image()
+        logo_text_image_rgba = self._get_text_image(text=logo_text, text_color=text_color)
 
         left_image = self._resize(image=image_rgba_1, max_size=max_size_l)
         right_image = self._resize(image=image_rgba_2, max_size=max_size_r)
         middle_image = self._resize(image=heart_image_rgba, max_size=size_m)
-        logo_image = self._resize(image=text_image_rgba, max_size=max_size_logo, margin=margin // 10)
+        logo_icon_image = self._resize(image=logo_icon_image_rgba, max_size=max_size_logo, margin=margin // 5)
+        logo_text_image = self._resize(image=logo_text_image_rgba, max_size=max_size_logo, margin=margin // 5)
 
         pasted_image = Image.new(mode="RGBA", size=max_size.tuple)
         pasted_image.paste(
@@ -55,7 +58,8 @@ class ConbinedImage:
 
         conbined_image = Image.new(mode="RGBA", size=max_size.tuple, color=bg_color)
         conbined_image.paste(im=pasted_image, box=(margin // 2, margin // 2))
-        conbined_image.paste(im=logo_image, box=(margin // 2, max_size.h - margin // 2))
+        conbined_image.paste(im=logo_icon_image, box=(margin // 2, max_size.h - margin // 2 + margin // 10))
+        conbined_image.paste(im=logo_text_image, box=(margin // 2 + logo_icon_image.width, max_size.h - margin // 2 + margin // 10))
         self._image = conbined_image
 
     @property
@@ -91,9 +95,14 @@ class ConbinedImage:
         return heart_image
 
     @staticmethod
+    def _get_logo_image() -> Image.Image:
+        logo_image = Image.open(fp=LOGO_IMAGE_PATH).convert(mode="RGBA")
+        return logo_image
+
+    @staticmethod
     def _get_text_image(text: str, text_color: str) -> Image.Image:
-        text_image = Image.new(mode="RGBA", size=(2000, 120))
+        text_image = Image.new(mode="RGBA", size=(2000, 90))
         draw = ImageDraw.Draw(im=text_image)
         font = ImageFont.truetype(font=FONT_PATH, size=80)
-        draw.text(xy=(1, 1), text=text, fill=text_color, font=font)
+        draw.text(xy=(1, 5), text=text, fill=text_color, font=font)
         return text_image
